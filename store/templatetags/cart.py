856f8d992abfc4c2b40f687b9d617d7f53c63a47
shikha2235 <shikha2235@gmail.com>
2021-05-02 11:19:39 +0530

    First Commit

---
+++ store/templatetags/cart.py
@@ -0,0 +1,40 @@
+from django import template
+from store.models.customer import Customer
+register = template.Library()
+
+@register.filter(name='is_in_cart')
+def is_in_cart(product  , cart):
+    keys = cart.keys()
+    for id in keys:
+        if int(id) == product.id:
+            return True
+    return False;
+
+
+@register.filter(name='cart_quantity')
+def cart_quantity(product, cart):
+    keys = cart.keys()
+    for id in keys:
+        if int(id) == product.id:
+            return cart.get(id)
+    return 0;
+
+
+@register.filter(name='price_total')
+def price_total(product  , cart):
+    return product.price * cart_quantity(product , cart)
+
+
+@register.filter(name='total_cart_price')
+def total_cart_price(products , cart):
+    sum = 0 ;
+    for p in products:
+        sum += price_total(p , cart)
+
+    return sum
+
+@register.filter(name='customer_name')
+def customer_name(customer):
+
+
+    return Customer.get_customer_by_id(int(customer)).first_name
---
+++ store/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models.product import Product
+from .models.category import Category
+# Create your views here.
+
+def index(request):
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products();
+
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+
+    # print('you are : ', request.session.get('customer'))
+    return render(request, 'index.html', data)
